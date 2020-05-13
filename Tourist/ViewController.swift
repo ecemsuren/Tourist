17aa17b63bfcfbbba037947b5bc3a3b1930925c4
ecemsuren <ecemsurenn@gmail.com>
2020-05-13 21:03:39 +0300

    Initial Commit

---
+++ Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tourist
+//
+//  Created by Ecem Aleyna on 13.05.2020.
+//  Copyright © 2020 Ecem Aleyna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
